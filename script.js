284fe5983033cda825b33d8c326a35d510207a75
doingshii <146015628+doingshii@users.noreply.github.com>
2024-02-06 21:13:37 +0000

    sonic can now jump

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+let timeout;
+
+window.addEventListener("keydown", checkKeyPressed, false);
+window.addEventListener("keydown", checkKeyPressed, false);
+window.addEventListener("keydown", checkKeyPressed, false);
 window.addEventListener("keydown", checkKeyPressed, false);
 
 function checkKeyPressed(evt) {
-    if (evt.keyCode == "38") {
-        alert("You pressed 'up'.");
-    }
+  if(evt.keyCode == "38"){
+    document.getElementById("sonic").src="sonic/sonic_up.jpg"
+  myFunction()}
+  
+  
+}
+
+
+
+function myFunction() {
+  timeout = setTimeout(next, 500);
+}
+
+function next()
+{
+  document.getElementById("sonic").src="sonic/sonic_reg.jpg"
 }
