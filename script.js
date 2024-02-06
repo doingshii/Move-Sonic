060ca5648baeca79f1b374bc85dd54d4715ddf15
doingshii <146015628+doingshii@users.noreply.github.com>
2024-02-06 21:19:36 +0000

    sonic can now duck and stop on release

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ window.addEventListener("keydown", checkKeyPressed, false);
 window.addEventListener("keydown", checkKeyPressed, false);
 window.addEventListener("keydown", checkKeyPressed, false);
 
+window.addEventListener("keyup", release, false);
+
+
 function checkKeyPressed(evt) {
   if(evt.keyCode == "38"){
     document.getElementById("sonic").src="sonic/sonic_up.jpg"
   myFunction()}
-  
-  
+  else if(evt.keyCode == "40")
+  {
+  document.getElementById("sonic").src="sonic/sonic_down.jpg"
+
+  }
 }
 
-
+function release(evt)
+  {
+    if(evt.keyCode=="40")
+  document.getElementById("sonic").src="sonic/sonic_reg.jpg"
+  }
 
 function myFunction() {
   timeout = setTimeout(next, 500);
